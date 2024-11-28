769e7fb25753278209c2750b89e55c1d6213dcdb
calc001 <christian.calleja@medirect.com.mt>
2024-11-28 16:20:52 +0100

    Included workaround fix

--- uiPagerExampleVue3/src/components/Home.vue
+++ uiPagerExampleVue3/src/components/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <Page>
     <StackLayout>
-      <GridLayout class="page" rows="*,auto">
+      <!-- EXAMPLE OF HOW IT SHOULD WORK, BUT DOESNT -->
+      <GridLayout class="page" rows="200,10">
         <Pager
           ref="pager"
           id="pager"
@@ -29,6 +30,36 @@
           marginBottom="10"
         />
       </GridLayout>
+      <!-- EXAMPLE OF WORKAROUND -->
+      <GridLayout class="page" rows="200">
+        <Pager
+          ref="pager2"
+          id="pager2"
+          width="100%"
+          android:width="100%"
+          :selectedIndex="selectedPage"
+          @selectedIndexChange="onPage2Changed"
+        >
+          <PagerItem
+            v-for="(item, index) in items"
+            :key="index"
+            :backgroundColor="item.color"
+          >
+            <StackLayout>
+              <Label :text="item.title" />
+            </StackLayout>
+          </PagerItem>
+        </Pager>
+        <!-- Try removing PAGERVIEWID to see the indicator actual position and how where it should be-->
+        <PagerIndicator
+          ref="pager2indicator"
+          type="worm"
+          verticalAlignment="bottom"
+          horizontalAlignment="center"
+          marginBottom="10"
+          @loaded="onPager2Load"
+        />
+      </GridLayout>
     </StackLayout>
   </Page>
 </template>
@@ -49,6 +80,18 @@ export default {
   methods: {
     onPageChanged: function (e) {
       this.selectedPage = e.value; //VUE3:: changed from e.selectedIndex to e.value
+    },
+    onPage2Changed: function (e) {
+      this.selectedPage = e.value; //VUE3:: changed from e.selectedIndex to e.value
+      this.$refs.pager2indicator?.nativeView.setSelection(
+        this.selectedPage,
+        true
+      );
+    },
+    onPager2Load(args) {
+      const component = args.object;
+      component.setCount(this.items.length);
+      component.setSelection(this.selectedPage, false);
     },
   },
 };
